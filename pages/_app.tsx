b2d69c87463399e160024b7f0eb92a7ad7739490
Miguel Hurtado <migue.ahg16@gmail.com>
2025-04-29 15:43:02 -0400

    Fix: add AppProps typing to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app'; // <-- Import AppProps types from Next.js
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
