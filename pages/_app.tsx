8c18cda7d64d8f36725deae3ceb9d82142f906df
Miguel Hurtado <migue.ahg16@gmail.com>
2025-04-29 19:02:25 -0400

    Added Vercel Speed Insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { AppProps } from 'next/app'; // <-- Import AppProps types from Next.js
 
 export default function App({ Component, pageProps }: AppProps) {
