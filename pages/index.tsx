67d29c43a6fb180ec9ed0ef9893fe993d7ce1de0
Miguel Hurtado <migue.ahg16@gmail.com>
2025-04-29 20:00:27 -0400

    Basic SEO optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,8 @@ export default function Home() {
     <>
       <Head>
         <title>Professional Web Design | GraphicWorks</title>
-        <meta name="description" content="We build stunning, responsive websites for businesses." />
+        <meta name="description" content="Professional web design services for small businesses, startups, and creators." />
+        <link rel="canonical" href="https://yourdomain.com" />
       </Head>
 
       <Navbar />
